254d2b47ff0880b2d47b01044339dfbf81de828b
Jacob <jacobgranberry@gmail.com>
2020-06-03 08:54:39 -0500

    more adjustments

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-import data from './WesterosBlocks.json';
-var fs = require('fs');
+import data from "./WesterosBlocks.json";
+var fs = require("fs");
 
-function flatten(data) {
-    var result = {};
-    function recurse (cur, prop) {
-        if (Object(cur) !== cur) {
-            result[prop] = cur;
-        } else if (Array.isArray(cur)) {
-             for(var i=0, l=cur.length; i<l; i++)
-                 recurse(cur[i], prop + "[" + i + "]");
-            if (l == 0)
-                result[prop] = [];
-        } else {
-            var isEmpty = true;
-            for (var p in cur) {
-                isEmpty = false;
-                recurse(cur[p], prop ? prop+"."+p : p);
-            }
-            if (isEmpty && prop)
-                result[prop] = {};
-        }
-    }
-    recurse(data, "");
-    return result;
-}
+const reducer = (input) => {
+  const result = [];
+  input.blocks.forEach((block) => {
+    block.subBlocks &&
+      block.subBlocks.forEach((subblock) => {
+        result.push({
+          name: subblock.label,
+          parentBlock: block.blockName,
+          blockID: block.blockID,
+          material: block.material,
+          sound: block.stepSound,
+          type: block.blockType,
+          light: block.lightValue,
+          meta: subblock.meta,
+          id: `${block.blockID}:${subblock.meta}`,
+          textures: subblock.textures,
+        });
+      });
+  });
 
+  return result;
+};
 
-const reducer = data.blocks.reduce((acc, curr) => {
-return curr.subBlocks.map((subblock) => {
-    return {
-        name: curr.blockName
-    }
-})
-}, {})
-
+console.log(reducer(data));
 
 // const reducer = data.blocks.map((block) => {
 //    return block.subBlocks.map((subblock) => {
@@ -54,16 +45,15 @@ return curr.subBlocks.map((subblock) => {
 //         }
 //     })
 // })
+
 const writeJson = () => {
-    fs.writeFile("./result.json", JSON.stringify(reducer, null, 4), (err) => {
-        if (err) {
-            console.error(err)
-            return;
-        };
-        console.log("File has been created");
-    });
-}
+  fs.writeFile("./result.json", JSON.stringify(result, null, 4), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("File has been created");
+  });
+};
 
 // writeJson()
-
-console.log(reducer)
